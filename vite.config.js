52adb7969f75b2a23b623ebf94e49be793031013
Dandy Nagel <github@dandynagel.com>
2025-11-20 13:31:59 +0100

    setup vite.config for aliases and two entry points

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        pokedex: path.resolve(__dirname, 'pokedex.html'),
+      },
+    },
+  },
 });
